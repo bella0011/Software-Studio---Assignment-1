1044f528b8074604462800d0d259457214ec1788
Danny-Vien <79494703+Danny-Vien@users.noreply.github.com>
2021-04-04 14:59:43 +1000

    added verifying register v2

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const User = require('../models/user')
+const bcrypt = require('bcryptjs');
 
 //Register Page
 router.get('/register',(req, res) => res.render('users/register'));
@@ -22,7 +23,7 @@ router.post('/register', (req, res) => {
     }
 
     if(errors.length > 0) {
-        res.render('register', {
+        res.render('users/register', {
             errors,
             name,
             email,
@@ -30,7 +31,42 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('pass')
+        // Validation Passed
+        User.findOne( {email: email})
+        .then(user => {
+            if(user) {
+                // User exists
+                errors.push({ msg: 'Email is already registered'})
+                res.render('users/register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                })
+
+                // Hash Password
+                bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        // Set password to hashed
+                        newUser.password = hash;
+                        // Save User
+                        newUser.save()
+                            .then(user => {
+                                res.redirect('/login')
+                            })
+                            .catch(err => console.log(err))
+
+                }))
+            }
+        })    
     }
 
 })
